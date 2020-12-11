8c9dba9db87d89ad75bf03f54638d0c70d0166dc
pahirdinhamut <fahreddin803@gmail.com>
2020-12-11 21:12:20 +0300

    düzenleme yaptım ve ListItem component ekledim

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,56 +1,73 @@
 import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
+import './style.css';
 
 class TodoList extends Component {
-	constructor(props) {
-		super(props);
-		// state componet durumu 
-		this.state = {
-			inputValue: '',
-			list: []
-		}
+  constructor(props) {
+    super(props);
+    // state componet durumu 
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+    this.handleTnputChange = this.handleTnputChange.bind(this);
+    this.handlebtnClick = this.handlebtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+  }
 
-	}
+  render() {
+    return (
+      <Fragment>
+        {/* bu rası  */}
+        <div>
+          <label htmlFor="inserArea" >İçerik Ekleyin</label>
+          <input id="inserArea" className='input' value={this.state.inputValue} onChange={this.handleTnputChange} />
+          <button onClick={this.handlebtnClick}>Gönder</button>
+        </div>
+        <ul>
+          {this.getTodoItem()}
+        </ul>
+      </Fragment >
+    )
+  }
 
-	render() {
-		return (
-			<Fragment>
-				<div>
-					<input value={this.state.inputValue} onChange={this.handleTnputChange.bind(this)} />
-					<button onClick={this.handlebtnClick.bind(this)}>Gönder</button>
-				</div>
-				<ul>
-					{this.state.list.map((item, index) => {
-						return <li key={index}
-							onClick={this.handleItemDelete.bind(this, index)}>{item}
-						</li>
-					})}
-				</ul>
-			</Fragment >
-		)
-	}
-	handleTnputChange(e) {
-		// setState asıl oldığu state verileri değiştimek için kullandık
-		this.setState({
-			inputValue: e.target.value
-		})
-	}
-	handlebtnClick() {
-		this.setState({
-			// list içindeki dizi verileri değiştirme 
-			list: [...this.state.list, this.state.inputValue],
-			inputValue: ''
-		})
-	}
-	handleItemDelete(index) {
-		//list dizisinin kopyasını aldık 
-		const list = [...this.state.list];
-		//list içindeki elemanlari indexleyip sildik 
-		list.splice(index, 1);
-		//sonra yeni list elemanlari değiştirdik 
-		this.setState({
-			list: list
-		})
-	}
+
+  getTodoItem() {
+
+    return this.state.list.map((item, index) => {
+      return (
+
+        <TodoItem key={index} content={item} index={index} deleteItem={this.handleItemDelete} />
+
+
+      )
+    })
+  }
+
+  handleTnputChange(e) {
+    const value = e.target.value;
+    // setState asıl oldığu state verileri değiştimek için kullandık
+    this.setState(() => ({
+      inputValue: value
+    }));
+  }
+  handlebtnClick() {
+    this.setState((provState) => ({
+      list: [...provState.list, provState.inputValue],
+      inputValue: ''
+    }));
+  }
+  handleItemDelete(index) {
+    this.setState((prevState) => {
+      //list dizisinin kopyasını aldık 
+      const list = [...prevState.list];
+      //list içindeki elemanlari indexleyip sildik 
+      list.splice(index, 1);
+      //sonra yeni list elemanlari değiştirdik  
+      return { list }
+    })
+
+  }
 }
 
-export default TodoList;
+export default TodoList; 
